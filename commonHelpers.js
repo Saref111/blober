18ee9cf71579a563a438a16d4acf1f2d7a42df48
Saref111 <50395293+Saref111@users.noreply.github.com>
2024-02-03 07:04:27 +0000

    Deploying to gh-pages from @ Saref111/blober@a23c03a79da24b39560088bc16968b5e424521dc 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,19 +1,18 @@
-import{b as l}from"./assets/vendor-84288bc5.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))i(r);new MutationObserver(r=>{for(const o of r)if(o.type==="childList")for(const s of o.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function n(r){const o={};return r.integrity&&(o.integrity=r.integrity),r.referrerpolicy&&(o.referrerPolicy=r.referrerpolicy),r.crossorigin==="use-credentials"?o.credentials="include":r.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(r){if(r.ep)return;r.ep=!0;const o=n(r);fetch(r.href,o)}})();const c=()=>`#${Math.floor(Math.random()*16777215).toString(16)}`,f=(t,e)=>Math.floor(Math.random()*(e-t+1))+t,u=t=>{const e=f(0,t.length-1);return t.splice(e,1)[0]},a=400,d=`<defs>
-                    <filter id="filter" x="-100" y="-100" width="750" height="750"
-                    filterUnits="userSpaceOnUse" col4or-interpolation-filters="sRGB">
-                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                        <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                        <feGaussianBlur stdDeviation="65.5" result="effect1_foregroundBlur_32_5690" />
-                    </filter>
-                </defs>`,m=["translate(75, 75)","translate(0, -75)","translate(-75, 0)"],g=t=>{const{path:e}=l({size:a,growth:1});return`<g transform="${u(m)}" filter="url(#filter)">
-                <path d="${e}" fill="${t}" >
+import{b as i}from"./assets/vendor-84288bc5.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function n(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerpolicy&&(r.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?r.credentials="include":t.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(t){if(t.ep)return;t.ep=!0;const r=n(t);fetch(t.href,r)}})();const u=()=>`#${Math.floor(Math.random()*16777215).toString(16)}`,c=(e,o)=>Math.floor(Math.random()*(o-e+1))+e,a=400,f=()=>`translate(${c(-300,300)}, ${c(-300,300)})`,p=e=>{const{path:o}=i({size:a,growth:1});return`<g transform="${f()}" filter="url(#filter)">
+                <path d="${o}" fill="${e}" >
                     <animate 
                         attributeName="d" 
                         dur="10s" 
                         repeatCount="indefinite" 
                         keyTimes="0;0.33;0.67;1" 
-                        values="${e};${l({size:a,growth:1.5}).path};${l({size:a,growth:1.5}).path};${e}" 
+                        values="${o};${i({size:a,growth:1.5}).path};${i({size:a,growth:1.5}).path};${o}" 
                     />
                 </path>
-            </g>`},p=(t={},e)=>{t.color||(t.color=c());const n=g(t.color);e.insertAdjacentHTML("afterbegin",n)},h=t=>{const e=document.querySelector(".screen");e.innerHTML="",e.insertAdjacentHTML("beforeend",d),t.forEach(n=>{p(n,e)})},y=t=>{const e=new FormData(t),n=[];for(const[i,r]of e.entries()){const[o,s]=i.split("_");n[s]||(n[s]={}),n[s][o]=r}return n},b=(t,e)=>{t.addEventListener("submit",n=>{n.preventDefault();const i=y(t);e(i)})},L=document.querySelector(".controls__form");b(L,h);
+            </g>`},g=(e={},o)=>{e.color||(e.color=u());const n=p(e.color);o.insertAdjacentHTML("afterbegin",n)},b=e=>{const o=document.querySelector(".screen");o.innerHTML="",e.forEach(n=>{g(n,o)})},m=e=>{const o=new FormData(e),n=[];for(const[s,t]of o.entries()){const[r,l]=s.split("_");n[l]||(n[l]={}),n[l][r]=t}return n},d=e=>`
+        <fieldset class="blob-fieldset">
+            <label>
+                <span>Color:</span>
+                <input value="${u()}" type="color" name="color_${e}" id="color_${e}" />
+            </label>
+        </fieldset>`,h=(e,o)=>{e.querySelector("#add-blob").addEventListener("click",()=>{const s=e.querySelectorAll(".blob-fieldset").length;e.insertAdjacentHTML("beforeend",d(s))}),e.addEventListener("submit",s=>{s.preventDefault();const t=m(e);o(t)}),e.insertAdjacentHTML("beforeend",d(0))},y=document.querySelector(".controls__form");h(y,b);
 //# sourceMappingURL=commonHelpers.js.map
