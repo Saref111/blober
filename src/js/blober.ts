274e7c3d3b9b06b6158bba51ef0e9600bdc08032
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-03-15 09:39:24 +0200

    added render

--- src/js/blober.ts
+++ src/js/blober.ts
@@ -1,61 +1,60 @@
-
-import { HexColor, generateHexColor, getRandomInt, removeRandomFromArray } from "./helpers";
+import { HexColor, generateHexColor } from './helpers';
 
 export type BlobConfig = {
-    color: HexColor;
-    id: string;
-    path: string;
-    transform: string;
-    filterId: string;
-    animationPaths: string[];
-    seed: number;
+  color: HexColor;
+  id: string;
+  path: string;
+  transform: string;
+  filterId: string;
+  animationPaths: string[];
+  seed: number;
 };
 
 const FILTER = `<defs>
                    
                 </defs>`;
 
-                // <filter id="filter" x="-100" y="-100" width="750" height="750"
-                // filterUnits="userSpaceOnUse" col4or-interpolation-filters="sRGB">
-                //     <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                //     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-                //     <feGaussianBlur stdDeviation="65.5" result="effect1_foregroundBlur_32_5690" />
-                // </filter>
+// <filter id="filter" x="-100" y="-100" width="750" height="750"
+// filterUnits="userSpaceOnUse" col4or-interpolation-filters="sRGB">
+//     <feFlood flood-opacity="0" result="BackgroundImageFix" />
+//     <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+//     <feGaussianBlur stdDeviation="65.5" result="effect1_foregroundBlur_32_5690" />
+// </filter>
 
 const getPathString = (cfg: BlobConfig) => {
-
-    return `<g transform="${cfg.transform}" filter="url(#${cfg.filterId})">
+  return `<g transform="${cfg.transform}" filter="url(#${cfg.filterId})">
                 <path d="${cfg.path}" fill="${cfg.color}" >
                     <animate 
                         attributeName="d" 
                         dur="10s" 
                         repeatCount="indefinite" 
                         keyTimes="0;0.33;0.67;1" 
-                        values="${cfg.path};${cfg.animationPaths.join(';')};${cfg.path}" 
+                        values="${cfg.path};${cfg.animationPaths.join(';')};${
+    cfg.path
+  }" 
                     />
                 </path>
             </g>`;
-        };
+};
 
 const generateBlob = (config: BlobConfig, svg: HTMLElement) => {
-    if (!config.color) {
-        config.color = generateHexColor();
-    }
+  if (!config.color) {
+    config.color = generateHexColor();
+  }
 
-    const blob = getPathString(config);
+  const blob = getPathString(config);
 
-    svg.insertAdjacentHTML('afterbegin', blob);
-}
+  svg.insertAdjacentHTML('afterbegin', blob);
+};
 
 export const generateBlobs = (blobConfigs: BlobConfig[]) => {
-    const svg = document.querySelector(".screen") as HTMLElement;
-    svg.innerHTML = "";
-    svg.insertAdjacentHTML('beforeend', FILTER);
+  const svg = document.querySelector('.screen') as HTMLElement;
+  svg.innerHTML = '';
+  svg.insertAdjacentHTML('beforeend', FILTER);
 
-    blobConfigs.forEach((config) => {
-        console.log(config);
-        
-        generateBlob(config, svg);
-    });
-    
+  blobConfigs.forEach(config => {
+    console.log(config);
+
+    generateBlob(config, svg);
+  });
 };
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { generateBlobs } from "./js/blober.js";
 import { handleExportSVG } from "./js/exportSVG.js";
 import { formHandler } from "./js/formHandler.js";
+import { blobStorage } from "./js/localStorageController.js";
 
 const form = document.querySelector(".controls__form") as HTMLFormElement;
 handleExportSVG();
 
 formHandler(form);
+
+document.addEventListener("update", () => {
+    generateBlobs(blobStorage.getEntities());
+});
+generateBlobs(blobStorage.getEntities());
