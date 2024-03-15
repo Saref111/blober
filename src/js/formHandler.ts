f996b7d64914974c03dfc526360c6371e10d9585
Ivan Borovyk <Ivan_Borovyk@epam.com>
2024-03-15 09:30:32 +0200

    added remove button

--- src/js/formHandler.ts
+++ src/js/formHandler.ts
@@ -2,19 +2,33 @@ import { BlobConfig } from './blober';
 import { blobStorage } from './localStorageController';
 import { generateNewBlobConfig } from './configBuilder';
 
-const getFieldset = ({id, color, seed}: BlobConfig) => {
-  return `
-        <fieldset class="blob-fieldset">
-            <label>
-                <span>Color:</span>
-                <input value="${color}" type="color" name="color_${id}" id="color_${id}" />
-            </label>
-            <label>
-              <span>Seed:</span>
-              <input value="${seed}" type="text" name="seed_${id}" id="seed_${id}" />
-            </label>
-            <button class="remove-blob" data-id="${id}">Remove TODO</button>
-        </fieldset>`;
+const removeBlob = (id: string) => {
+  const blob = blobStorage.findEntity((blob: BlobConfig) => blob.id === id);
+  if (!blob) {
+    return;
+  }
+  blobStorage.removeEntity(blob);
+};
+
+const getFieldset = ({ id, color, seed }: BlobConfig) => {
+  const fieldset = document.createElement('fieldset');
+  fieldset.classList.add('blob-fieldset');
+  fieldset.innerHTML = `
+      <label>
+          <span>Color:</span>
+          <input value="${color}" type="color" name="color_${id}" id="color_${id}" />
+      </label>
+      <label>
+        <span>Seed:</span>
+        <input value="${seed}" type="text" name="seed_${id}" id="seed_${id}" />
+      </label>
+      <button type="button" class="remove-blob">Remove</button>
+  `;
+  const removeButton = fieldset.querySelector(
+    '.remove-blob'
+  ) as HTMLButtonElement;
+  removeButton.addEventListener('click', () => removeBlob(id));
+  return fieldset;
 };
 
 export const formHandler = (form: HTMLFormElement) => {
@@ -25,7 +39,7 @@ export const formHandler = (form: HTMLFormElement) => {
     const blobs = blobStorage.getEntities();
     form.querySelectorAll('.blob-fieldset').forEach(el => el.remove());
     blobs.forEach(blob => {
-      form.insertAdjacentHTML('beforeend', getFieldset(blob));
+      form.insertAdjacentElement('beforeend', getFieldset(blob));
     });
   };
 
